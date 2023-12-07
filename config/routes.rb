0d6e48fbd79ade7805c9e91ef5fcd08e2d1e8a0a
yogi1116 <tbs.yonenchu.283@icloud.com>
2023-12-07 15:31:49 +0900

    staticからtopに名前を変更

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  root 'static_pages#top'
+  root 'top_pages#top'
   resources :posts, only: %i[index new create]
 end
