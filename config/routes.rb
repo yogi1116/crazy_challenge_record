0e754e93233eee4f7bd5aa07adfe807df641e3e0
yogi1116 <tbs.yonenchu.283@icloud.com>
2023-12-09 22:01:47 +0900

    ルーティングの変更（post関係の削除)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'static_pages/top'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -7,5 +6,4 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   root 'top_pages#top'
-  resources :posts, only: %i[index new create]
 end
