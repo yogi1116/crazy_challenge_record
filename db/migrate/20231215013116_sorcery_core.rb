e7b05dabe30dcb97d28d9fc15ee6e1ae720a3295
yogi1116 <tbs.yonenchu.283@icloud.com>
2023-12-15 17:29:21 +0900

    userモデルとマイグレーションファイルにバリデーションを追加

--- db/migrate/20231215013116_sorcery_core.rb
+++ db/migrate/20231215013116_sorcery_core.rb
@@ -2,8 +2,9 @@ class SorceryCore < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
       t.string :email,            null: false, index: { unique: true }
-      t.string :crypted_password
+      t.string :crypted_password, null: false
       t.string :salt
+      t.string :username,         null: false
 
       t.timestamps                null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_27_135119) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_15_013116) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,16 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_27_135119) do
     t.text "body", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "crypted_password", null: false
+    t.string "salt"
+    t.string "username", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
