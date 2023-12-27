2ef6779eb3e1a54b1090b7e58d3f0215b48d8ea8
yogi1116 <tbs.yonenchu.283@icloud.com>
2023-12-27 11:02:56 +0900

    add:ログアウト機能導入

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,9 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   root 'top_pages#top'
-  get '/login', to: 'user_sessions#new'
-  post '/login', to: 'user_sessions#create'
+  get  '/login',  to: 'user_sessions#new'
+  post '/login',  to: 'user_sessions#create'
+  delete '/logout', to: 'user_sessions#destroy'
 
   resources :users, only: %i[new create]
 end
