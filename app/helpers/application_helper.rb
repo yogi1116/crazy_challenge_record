579667bf49ae90df295c271d583f6f68a9f8f412
yogi1116 <tbs.yonenchu.283@icloud.com>
2024-01-05 03:34:34 +0900

    add:flashメッセージのレイアウト完了

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,3 @@
 module ApplicationHelper
-  def flash_background_color(type)
-    case type.to_sym
-    when :success then "bg-sky-300"
-    when :notice  then "bg-green-500"
-    when :error  then "bg-red-500"
-    else "bg-gray-500"
-    end
-  end
+  include FlashHelper
 end
