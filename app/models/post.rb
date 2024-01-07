1cbf3537d680dd523a9837ec4df41cb724b6969d
yogi1116 <tbs.yonenchu.283@icloud.com>
2024-01-08 01:15:50 +0900

    add:アップロード画像の枚数制限をpostモデル(image_count_within_limitメソッド)に追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,8 +6,17 @@ class Post < ApplicationRecord
   validates :content, presence: true
   validates :category, presence: true
   validates :retry, presence: true, if: -> { challenge_result == 'give_up' }
+  validate :image_count_within_limit
 
   enum challenge_result: { complete: 0, give_up: 1 }
   enum category: { sports: 0, patience: 1, study: 2 }
   enum retry: { try: 0, no_try: 1 }
+
+  private
+
+  def image_count_within_limit
+    if images.length > 4
+      errors.add(:images, 'に添付できるのは大4枚までです。')
+    end
+  end
 end
