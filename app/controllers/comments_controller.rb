828e4a5d3867b7707cce03cef3c923ec6efd3d44
yogi1116 <tbs.yonenchu.283@icloud.com>
2024-01-17 07:44:59 +0900

    add:コメント編集機能の追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,18 @@
 class CommentsController < ApplicationController
-  before_action :set_post, only: %i[create destroy]
+  before_action :set_post, only: %i[create edit update destroy]
 
   def create
     @comment = @post.comments.build(comment_params.merge(user: current_user))
     @comment.save!
+  end
+
+  def edit
+    @comment = current_user.comments.find(params[:id])
+  end
+
+  def update
+    @comment = current_user.comments.find(params[:id])
+    @comment.update!(comment_params)
   end
 
   def destroy
