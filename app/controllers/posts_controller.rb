c04c6533e4c5b8450563ea8fa5b3dfa41fbc3b20
yogi1116 <tbs.yonenchu.283@icloud.com>
2024-01-17 18:03:05 +0900

    add:ランキングページの初期レイアウト作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -44,6 +44,15 @@ class PostsController < ApplicationController
     redirect_to posts_path, flash: { success: t('posts.destroy.success') }
   end
 
+  def ranking
+    @posts = Post.joins(:likes)
+                  .select('posts.*, COUNT(likes.id) AS likes_count')
+                  .group('posts.id')
+                  .order('likes_count DESC')
+                  .limit(10)
+                  .includes(:user)
+  end
+
   private
 
   def post_params
