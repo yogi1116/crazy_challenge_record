b639582cd2972ee20f6a1f458be67fa339f8c075
yogi1116 <tbs.yonenchu.283@icloud.com>
2024-02-03 22:37:11 +0900

    add:HEIC形式画像のリサイズロジック追加

--- app/controllers/concerns/image_processing_concern.rb
+++ app/controllers/concerns/image_processing_concern.rb
@@ -4,12 +4,21 @@ module ImageProcessingConcern
   def process_image(image, width: 800, height: 800)
     return unless image
 
-    processed_image = ::ImageProcessing::Vips
-                        .source(image.tempfile)
-                        .resize_to_limit(width, height)
-                        .saver(quality: 80)
-                        .call
+    if image.content_type == 'image/heic'
+      processed_image = ::ImageProcessing::Vips
+                          .source(image.tempfile)
+                          .convert('jpg') # HEICをJPEGに変換
+                          .resize_to_limit(width, height)
+                          .saver(quality: 80)
+                          .call
+    else
+      processed_image = ::ImageProcessing::Vips
+                          .source(image.tempfile)
+                          .resize_to_limit(width, height)
+                          .saver(quality: 80)
+                          .call
+    end
 
-    { io: File.open(processed_image.path), filename: image.original_filename, content_type: image.content_type }
+    { io: File.open(processed_image.path), filename: "#{File.basename(image.original_filename, '.*')}.jpg", content_type: 'image/jpeg' }
   end
 end
