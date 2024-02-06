95bc163404cda7838e82d70230c0347360534f8f
yogi1116 <tbs.yonenchu.283@icloud.com>
2024-02-06 22:33:44 +0900

    add:authenticationモデルとマイグレーションファイルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :liked_posts, through: :likes, source: :post
   has_many :comments, dependent: :destroy
+  has_many :authentications, :dependent => :destroy
+  accepts_nested_attributes_for :authentications
 
   validates :username, presence: true, length: { maximum: 40 }
   validates :email, presence: true, uniqueness: true
