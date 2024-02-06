a7ea9dfc19e1c24a5d5e017b0c62bbdbf391238e
yogi1116 <tbs.yonenchu.283@icloud.com>
2024-02-06 22:59:40 +0900

    add:oauthsコントローラとルーティングの設定

---
+++ app/controllers/oauths_controller.rb
@@ -0,0 +1,28 @@
+class OauthsController < ApplicationController
+  skip_before_action :require_login
+
+  def oauth
+    login_at(auth_params[:provider])
+  end
+
+  def callback
+    provider = auth_params[:provider]
+    if (@user = login_from(provider))
+      redirect_to posts_path
+    else
+      begin
+        @user = create_from(provider)
+        reset_session
+        auto_login(@user)
+        redirect_to root_path
+      rescue StandardError
+        redirect_to root_path
+    end
+  end
+
+  private
+
+  def auth_params
+    params.permit(:code, :provider)
+  end
+end
