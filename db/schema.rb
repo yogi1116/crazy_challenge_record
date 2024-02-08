b4555baada5809a77556c8703184f57818226fd0
yogi1116 <tbs.yonenchu.283@icloud.com>
2024-02-08 17:42:29 +0900

    add:usersテーブルのnull制約を解除するためテーブル追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_06_131102) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_08_083008) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -118,8 +118,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_06_131102) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", null: false
-    t.string "crypted_password", null: false
+    t.string "email"
+    t.string "crypted_password"
     t.string "salt"
     t.string "username", null: false
     t.datetime "created_at", null: false
