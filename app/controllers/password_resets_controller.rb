060053710c07e1a9968b9ec553f7607fa4984575
yogi1116 <tbs.yonenchu.283@icloud.com>
2024-02-12 04:30:09 +0900

    add:update時のバリデーション機能追加とエラーメッセージの文字色カスタマイズ

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,5 +1,5 @@
 class PasswordResetsController < ApplicationController
-  skip_before_action :require_login
+  skip_before_action :require_login, only: %i[create edit update new]
 
   def new; end
 
@@ -20,13 +20,26 @@ class PasswordResetsController < ApplicationController
     @user = User.load_from_reset_password_token(@token)
 
     return not_authenticated if @user.blank?
+    return if validation_reset_password
 
     @user.password_confirmation = params[:user][:password_confirmation]
     if @user.change_password(params[:user][:password])
-      redirect_to login_path, success: t('.success')
+      redirect_to login_path, flash: { success: t('.success') }
+      return
     else
-      flash.now[:danger] = t('.fail')
-      render :edit
+      flash.now[:error] = t('.fail')
+      render :edit, status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def validation_reset_password
+    if params[:user][:password].blank? || params[:user][:password_confirmation].blank?
+      flash.now[:error] = t('.empty_password')
+      render :edit, status: :unprocessable_entity
+      return true
+    end
+    false
   end
 end
