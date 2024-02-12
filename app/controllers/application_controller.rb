77b35f4f5b09e0fe05e0e8a9966e3a334f15d371
yogi1116 <tbs.yonenchu.283@icloud.com>
2024-02-13 03:23:40 +0900

    fix:@profile部分の修正とset_profileは使用しないため削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :require_login
-  before_action :set_profile
 
   private
 
   def not_authenticated
     flash[:notice] = t('defaults.require_login')
     redirect_to login_path
-  end
-
-  def set_profile
-    @profile = current_user.profile if logged_in?
   end
 end
