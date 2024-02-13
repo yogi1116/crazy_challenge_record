f01c8e4dc04c6d853220520edba823306d26f0bb
yogi1116 <tbs.yonenchu.283@icloud.com>
2024-02-13 23:13:25 +0900

    fix:保存済みユーザーのuuid適用とルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   get '/privacy_policy', to: 'top_pages#privacy_policy'
   get '/terms_of_service', to: 'top_pages#terms_of_service'
 
-  resources :users, only: %i[new create] do
+  resources :users, param: :uuid, only: %i[new create] do
     resource :profile, only: [:show]
   end
   resource :profile, only: %i[show edit update]
