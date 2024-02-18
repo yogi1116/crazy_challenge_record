544d954faeb06fb0c1c2b3586ccbb3f9682b18eb
yogi1116 <tbs.yonenchu.283@icloud.com>
2024-02-19 00:24:27 +0900

    fix:iOS版での背景画像固定を適用させるための修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,24 +12,6 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
-      backgroundImage: theme => ({
-        'bgfixed': "url('/assets/img/bgfixed.jpg')",
-      }),
-      backgroundPosition: {
-        'bgfixed': 'center',
-      },
-      backgroundSize: {
-        'bgfixed': 'cover',
-      },
-      backgroundAttachment: {
-        'bgfixed': 'fixed',
-      },
-      backgroundRepeat: {
-        'bgfixed': 'no-repeat',
-      },
-      height: {
-        'bgfixed': '500px',
-      }
     },
   },
   plugins: [
