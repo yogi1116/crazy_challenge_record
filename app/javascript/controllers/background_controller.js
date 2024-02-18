ac0734933a060136f9bd92c575dd3ef9531041a3
yogi1116 <tbs.yonenchu.283@icloud.com>
2024-02-18 09:03:53 +0900

    add:background_controller.jsの作成

---
+++ app/javascript/controllers/background_controller.js
@@ -0,0 +1,11 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  connect() {
+    this.element.style.backgroundImage = "url('<%= image_path('post.jpeg') %>')";
+    this.element.style.backgroundPosition = 'center';
+    this.element.style.backgroundRepeat = 'no-repeat';
+    this.element.style.backgroundSize = 'cover';
+    this.element.style.backgroundAttachment = 'fixed';
+  }
+}
