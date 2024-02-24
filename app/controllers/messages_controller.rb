c24dce51635cfbed90702ba09eb3a302e03e445e
yogi1116 <tbs.yonenchu.283@icloud.com>
2024-02-24 20:44:55 +0900

    add:action cableの実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,13 +7,13 @@ class MessagesController < ApplicationController
     @message = current_user.sent_messages.build(message_params)
     if @message.save
       ChatChannel.broadcast_to(
-        @message.receiver,  # メッセージの受信者を指定
-        { message: @message.as_json, sender: current_user.username }  # メッセージと送信者の情報をJSON形式で送る
+        @message.receiver,
+        { message: @message.as_json(include: { user: { only: :username } }), type: 'message' }
       )
     else
       ChatChannel.broadcast_to(
-        current_user,  # 現在のユーザー（メッセージの送信者）にエラーを通知
-        { error: @message.errors.full_messages.to_sentence }  # エラーメッセージを含むJSONオブジェクト
+        current_user,
+        { error: @message.errors.full_messages.to_sentence }
       )
     end
   end
@@ -25,37 +25,11 @@ class MessagesController < ApplicationController
                               current_user.id, receiver_id, receiver_id, current_user.id)
     @message = Message.new
     @message.receiver_id = @receiver.id
-
-    last_message_id = params[:last_message_id]
-    if last_message_id
-      additional_messages = Message.where("id < ?", last_message_id).limit(10)
-      @messages = @messages.or(additional_messages)
-    end
-
-    respond_to do |format|
-      format.html
-      format.turbo_stream
-    end
-  end
-
-  def destory
-
   end
 
   private
 
   def message_params
     params.require(:message).permit(:body, :receiver_id)
-  end
-
-  def prepare_messages
-    @messages = current_user.sent_messages
-  end
-
-  def respond_with_error
-    respond_to do |format|
-      format.turbo_stream { render turbo_stream: turbo_stream.replace("message_form", partial: "form", locals: { message: @message }) }
-      format.html { render :new }
-    end
   end
 end
--- app/javascript/channels/chat_channel.js
+++ app/javascript/channels/chat_channel.js
@@ -1,15 +1,22 @@
 import consumer from "./consumer"
 
-consumer.subscriptions.create("ChatChannel", {
+consumer.subscriptions.create({ channel: "ChatChannel", conversation_id: conversationId }, {
   connected() {
-    // Called when the subscription is ready for use on the server
+    console.log("チャンネルに接続しました");
   },
 
   disconnected() {
-    // Called when the subscription has been terminated by the server
+    console.log("チャンネルを切断しました");
   },
 
   received(data) {
-    // Called when there's incoming data on the websocket for this channel
+    if (data.message) {
+      const messages = document.getElementById('messages');
+      messages.innerHTML += `<p>${data.message.body}</p>`;
+    }
+
+    if (data.error) {
+      alert(data.error);
+    }
   }
 });
