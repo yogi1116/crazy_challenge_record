d978f37879bc19c1125315588d5e0381a9178775
yogi1116 <tbs.yonenchu.283@icloud.com>
2024-02-28 17:33:04 +0900

    fix:ログイン時にsessionを付与

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,24 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_verified_user
+    end
+
+    private
+
+    def find_verified_user
+      user_id = session["user_id"]
+      if user_id && (user = User.find_by_id(user_id))
+        user
+      else
+        reject_unauthorized_connection
+      end
+    end
+
+    def session
+      cookies.encrypted[Rails.application.config.session_options[:key]]
+    end
   end
 end
--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -1,11 +1,11 @@
 class ChatChannel < ApplicationCable::Channel
   def subscribed
-    receiver_id = params[:receiver_id]
+    receiver_id = params[:receiver_id].to_i
     private_chat_room = if current_user.id < receiver_id
-                    "chat_channel_#{current_user.id}_#{receiver_id}"
-                  else
-                    "chat_channel_#{receiver_id}_#{current_user.id}"
-                  end
+                          "chat_channel_#{current_user.id}_#{receiver_id}"
+                        else
+                          "chat_channel_#{receiver_id}_#{current_user.id}"
+                        end
 
     stream_from private_chat_room
   end
