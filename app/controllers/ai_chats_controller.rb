b90b4b99e68bd6bf20ef29ca65559c0303b9d953
yogi1116 <tbs.yonenchu.283@icloud.com>
2024-03-18 02:09:24 +0900

    add:セッションにチャット内容を24時まで保存機能追加

--- app/controllers/ai_chats_controller.rb
+++ app/controllers/ai_chats_controller.rb
@@ -1,11 +1,28 @@
 class AiChatsController < ApplicationController
-  def new; end
+  before_action :check_session_expiration
+
+  def new
+    @chat_bodies = session[:chat_body]
+  end
 
   def create
     service = OpenAiChatService.new([{role: "user", content: params[:body]}])
     @ai_response = service.chat
+
+    session[:chat_body] ||= []
+    session[:chat_body] << {user: params[:body], ai: @ai_response}
+    session[:expires_at] ||= Time.current.end_of_day.to_i
+
     respond_to do |format|
       format.turbo_stream
+    end
+  end
+
+  private
+
+  def check_session_expiration
+    if session[:expires_at].present? && Time.current.to_i > session[:expires_at]
+      reset_session
     end
   end
 end
