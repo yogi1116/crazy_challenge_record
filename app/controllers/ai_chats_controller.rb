9240c12d9a1d1d01b0f1946a7051926428b598ba
yogi1116 <tbs.yonenchu.283@icloud.com>
2024-03-19 10:16:47 +0900

    add:セッション保存からredis保存及び設定完了

--- app/controllers/ai_chats_controller.rb
+++ app/controllers/ai_chats_controller.rb
@@ -1,28 +1,54 @@
 class AiChatsController < ApplicationController
-  before_action :check_session_expiration
+  before_action :set_chat_limit_reached, only: [:new, :create]
 
   def new
-    @chat_bodies = session[:chat_body]
+    @chat_bodies = fetch_chat_bodies
   end
 
   def create
-    service = OpenAiChatService.new([{role: "user", content: params[:body]}])
-    @ai_response = service.chat
+    if @chat_limit_reached
+      @chat_bodies = fetch_chat_bodies
+      flash.now[:error] = '本日のチャット回数の上限に達しました。'
+      render :new, status: :unprocessable_entity
+    else
+      service = OpenAiChatService.new([{role: "user", content: params[:body]}])
+      @ai_response = service.chat
 
-    session[:chat_body] ||= []
-    session[:chat_body] << {user: params[:body], ai: @ai_response}
-    session[:expires_at] ||= Time.current.end_of_day.to_i
-
-    respond_to do |format|
-      format.turbo_stream
+      save_chat_body(params[:body], @ai_response)
+      respond_to do |format|
+        format.turbo_stream
+      end
     end
   end
 
   private
 
-  def check_session_expiration
-    if session[:expires_at].present? && Time.current.to_i > session[:expires_at]
-      reset_session
+  def set_chat_limit_reached
+    # ログインしているユーザーIDを基にしたカウントキーを使用
+    count_key = "chat_count:user:#{session[:user_id]}"
+    chat_count = $redis.get(count_key).to_i
+    @chat_limit_reached = chat_count >= 3
+    unless @chat_limit_reached
+      $redis.incr(count_key)
+      $redis.expire(count_key, seconds_until_end_of_day_jst)
     end
+  end
+
+  def fetch_chat_bodies
+    # ログインしているユーザーIDを基にしたチャットキーを使用
+    chat_key = "chat:user:#{session[:user_id]}"
+    $redis.lrange(chat_key, 0, -1).map { |body| JSON.parse(body) }
+  end
+
+  def save_chat_body(user_input, ai_response)
+    chat_key = "chat:user:#{session[:user_id]}"
+    $redis.rpush(chat_key, {user: user_input, ai: ai_response}.to_json)
+    $redis.expire(chat_key, seconds_until_end_of_day_jst)
+  end
+
+  def seconds_until_end_of_day_jst
+    now = Time.current.in_time_zone('Asia/Tokyo')
+    end_of_day = now.end_of_day
+    (end_of_day - now).to_i
   end
 end
---
+++ config/initializers/redis.rb
@@ -0,0 +1,2 @@
+$redis = Redis.new(url: ENV['REDIS_URL'] || 'redis://localhost:6379/1')
+
